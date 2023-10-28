38fe7767a53c0a22d1a499c53b33c47ca610f921
Nadav Kirsh <saturdaysyouth@gmail.com>
2023-10-28 16:06:22 +0200

    Change navlink to link

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { siteConfig } from "@/config/site";
-import NextLink from "next/link";
+import Link from "next/link";
 import clsx from "clsx";
 import { useTranslations } from "next-intl";
 import { usePathname } from "next/navigation";
@@ -19,7 +19,7 @@ export const Sidebar = ({ setIsOpen }: Props) => {
             <div className="p-4 flex-1 justify-center">
                 <div className="space-y-2">
                     {siteConfig.navItems.map((item) => (
-                        <NextLink
+                        <Link
                             onClick={() => setIsOpen && setIsOpen(false)}
                             className={clsx(
                                 "flex items-center hover:bg-muted",
@@ -31,7 +31,7 @@ export const Sidebar = ({ setIsOpen }: Props) => {
                             href={item.href}>
                             <item.icon className="mr-2 w-4 h-4" />
                             {t(item.label)}
-                        </NextLink>
+                        </Link>
                     ))}
                 </div>
             </div>
