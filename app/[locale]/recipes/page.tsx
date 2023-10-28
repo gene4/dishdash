d81e70e8815c112e3a562c8d7982d28279a814ed
Nadav Kirsh <saturdaysyouth@gmail.com>
2023-10-28 15:45:18 +0200

    test

--- app/[locale]/recipes/page.tsx
+++ app/[locale]/recipes/page.tsx
@@ -8,46 +8,46 @@ export default async function RecipesPage() {
     if (!userId) {
         return redirectToSignIn();
     }
-    const recipes = await prismadb.recipe.findMany({
-        where: {
-            userId,
-        },
-        include: {
-            ingredients: {
-                include: {
-                    ingredient: {
-                        select: {
-                            price: true,
-                            amount: true,
-                        },
-                    },
-                },
-            },
-        },
-    });
+    // const recipes = await prismadb.recipe.findMany({
+    //     where: {
+    //         userId,
+    //     },
+    //     include: {
+    //         ingredients: {
+    //             include: {
+    //                 ingredient: {
+    //                     select: {
+    //                         price: true,
+    //                         amount: true,
+    //                     },
+    //                 },
+    //             },
+    //         },
+    //     },
+    // });
 
-    const ingredients = await prismadb.ingredient.findMany({
-        where: {
-            userId,
-        },
-    });
+    // const ingredients = await prismadb.ingredient.findMany({
+    //     where: {
+    //         userId,
+    //     },
+    // });
 
-    const suppliers = await prismadb.supplier.findMany({
-        where: {
-            userId,
-        },
-    });
+    // const suppliers = await prismadb.supplier.findMany({
+    //     where: {
+    //         userId,
+    //     },
+    // });
 
     return (
         <>
             <h1 className="mt-10 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0  mb-5">
                 Recipes
             </h1>
-            <DataTable
+            {/* <DataTable
                 data={recipes}
                 ingredients={ingredients}
                 suppliers={suppliers}
-            />
+            /> */}
         </>
     );
 }
