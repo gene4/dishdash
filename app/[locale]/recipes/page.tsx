f6124ca7750768e253ffc173933d63ed01ec22c3
Nadav Kirsh <saturdaysyouth@gmail.com>
2023-10-28 16:04:11 +0200

    test

--- app/[locale]/recipes/page.tsx
+++ app/[locale]/recipes/page.tsx
@@ -1,55 +1,53 @@
-"use client";
-
 import { auth, redirectToSignIn } from "@clerk/nextjs";
 import prismadb from "@/lib/prismadb";
 import { DataTable } from "./data-table";
 
-export default function RecipesPage() {
-    // const { userId } = auth();
+export default async function RecipesPage() {
+    const { userId } = auth();
 
-    // if (!userId) {
-    //     return redirectToSignIn();
-    // }
-    // const recipes = await prismadb.recipe.findMany({
-    //     where: {
-    //         userId,
-    //     },
-    //     include: {
-    //         ingredients: {
-    //             include: {
-    //                 ingredient: {
-    //                     select: {
-    //                         price: true,
-    //                         amount: true,
-    //                     },
-    //                 },
-    //             },
-    //         },
-    //     },
-    // });
+    if (!userId) {
+        return redirectToSignIn();
+    }
+    const recipes = await prismadb.recipe.findMany({
+        where: {
+            userId,
+        },
+        include: {
+            ingredients: {
+                include: {
+                    ingredient: {
+                        select: {
+                            price: true,
+                            amount: true,
+                        },
+                    },
+                },
+            },
+        },
+    });
 
-    // const ingredients = await prismadb.ingredient.findMany({
-    //     where: {
-    //         userId,
-    //     },
-    // });
+    const ingredients = await prismadb.ingredient.findMany({
+        where: {
+            userId,
+        },
+    });
 
-    // const suppliers = await prismadb.supplier.findMany({
-    //     where: {
-    //         userId,
-    //     },
-    // });
+    const suppliers = await prismadb.supplier.findMany({
+        where: {
+            userId,
+        },
+    });
 
     return (
         <>
             <h1 className="mt-10 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0  mb-5">
                 Recipes
             </h1>
-            {/* <DataTable
+            <DataTable
                 data={recipes}
                 ingredients={ingredients}
                 suppliers={suppliers}
-            /> */}
+            />
         </>
     );
 }
