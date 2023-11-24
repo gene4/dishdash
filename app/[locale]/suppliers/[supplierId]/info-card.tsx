3498f0241608e636fe354af727624fe32f152eed
Nadav Kirsh <saturdaysyouth@gmail.com>
2023-11-24 21:00:24 +0100

    info card

--- app/[locale]/suppliers/[supplierId]/info-card.tsx
+++ app/[locale]/suppliers/[supplierId]/info-card.tsx
@@ -12,11 +12,6 @@ export default function InfoCard({ supplier }: Props) {
     return (
         <Card>
             <h1 className="text-2xl mb-5">Information</h1>
-            {supplier.paymentDay && (
-                <p className="mb-2 text-sm">
-                    Next payment date: {getNextPaymentDate(supplier.paymentDay)}
-                </p>
-            )}
             <p className="text-sm">{supplier.paymentInfo}</p>
         </Card>
     );
