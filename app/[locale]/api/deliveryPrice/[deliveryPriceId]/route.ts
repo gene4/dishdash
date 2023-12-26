ed657c8f805417afc683221e4eb1edb30e0fc02e
Nadav Kirsh <saturdaysyouth@gmail.com>
2023-12-26 22:14:20 +0200

    Delete variant field

--- app/[locale]/api/deliveryPrice/[deliveryPriceId]/route.ts
+++ app/[locale]/api/deliveryPrice/[deliveryPriceId]/route.ts
@@ -16,7 +16,6 @@ export async function PATCH(
             price,
             date,
             deliveryId,
-            variant,
             supplierId,
             ingredientId,
         } = body;
@@ -43,7 +42,6 @@ export async function PATCH(
                 supplierId,
                 amount,
                 price,
-                ingredientVariantId: variant,
                 unit,
                 deliveryId,
                 date: new Date(date || new Date()),
