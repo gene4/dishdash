a10ab1f1c009b4785170fbae478a28910387d8f8
Nadav Kirsh <saturdaysyouth@gmail.com>
2024-02-23 00:37:51 +0100

    disable data prefetch

--- app/[locale]/(platform)/(dashboard)/recipes/create-recipe/page.tsx
+++ app/[locale]/(platform)/(dashboard)/recipes/create-recipe/page.tsx
@@ -19,32 +19,32 @@ export default async function CreateRecipePage() {
         return redirectToSignIn();
     }
 
-    const queryClient = new QueryClient();
+    // const queryClient = new QueryClient();
 
-    // Prefetch data
-    const recipes = queryClient.prefetchQuery({
-        queryKey: ["recipes"],
-        queryFn: getRecipes,
-    });
+    // // Prefetch data
+    // const recipes = queryClient.prefetchQuery({
+    //     queryKey: ["recipes"],
+    //     queryFn: getRecipes,
+    // });
 
-    const ingredients = queryClient.prefetchQuery({
-        queryKey: ["ingredients"],
-        queryFn: getIngredients,
-    });
+    // const ingredients = queryClient.prefetchQuery({
+    //     queryKey: ["ingredients"],
+    //     queryFn: getIngredients,
+    // });
 
-    const suppliers = queryClient.prefetchQuery({
-        queryKey: ["suppliers"],
-        queryFn: getSuppliers,
-    });
+    // const suppliers = queryClient.prefetchQuery({
+    //     queryKey: ["suppliers"],
+    //     queryFn: getSuppliers,
+    // });
 
-    await Promise.all([recipes, ingredients, suppliers]);
+    // await Promise.all([recipes, ingredients, suppliers]);
 
     return (
         <>
             <h1 className="scroll-m-20 pb-2 text-2xl font-semibold tracking-tight transition-colors first:mt-0 mb-5">
                 Create Recipe
             </h1>
-            <HydrationBoundary state={dehydrate(queryClient)}>
+            <HydrationBoundary>
                 <CreateRecipeForm />
             </HydrationBoundary>
         </>
