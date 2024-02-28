ac0b4dba77b3fb6a8f5ed2955820e704e6dea3c4
Nadav Kirsh <saturdaysyouth@gmail.com>
2024-02-28 22:52:25 +0100

    disable data prefetch

--- app/[locale]/(platform)/(dashboard)/recipes/create-recipe/page.tsx
+++ app/[locale]/(platform)/(dashboard)/recipes/create-recipe/page.tsx
@@ -1,10 +1,4 @@
 import { auth, redirectToSignIn } from "@clerk/nextjs";
-import { getIngredients, getRecipes, getSuppliers } from "@/lib/actions";
-import {
-    HydrationBoundary,
-    QueryClient,
-    dehydrate,
-} from "@tanstack/react-query";
 import CreateRecipeForm from "@/components/recipes/create-recipe-form";
 import { redirect } from "next/navigation";
 
@@ -19,34 +13,12 @@ export default async function CreateRecipePage() {
         return redirectToSignIn();
     }
 
-    // const queryClient = new QueryClient();
-
-    // // Prefetch data
-    // const recipes = queryClient.prefetchQuery({
-    //     queryKey: ["recipes"],
-    //     queryFn: getRecipes,
-    // });
-
-    // const ingredients = queryClient.prefetchQuery({
-    //     queryKey: ["ingredients"],
-    //     queryFn: getIngredients,
-    // });
-
-    // const suppliers = queryClient.prefetchQuery({
-    //     queryKey: ["suppliers"],
-    //     queryFn: getSuppliers,
-    // });
-
-    // await Promise.all([recipes, ingredients, suppliers]);
-
     return (
         <>
             <h1 className="scroll-m-20 pb-2 text-2xl font-semibold tracking-tight transition-colors first:mt-0 mb-5">
                 Create Recipe
             </h1>
-            <HydrationBoundary>
-                <CreateRecipeForm />
-            </HydrationBoundary>
+            <CreateRecipeForm />
         </>
     );
 }
